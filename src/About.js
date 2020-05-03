73f059c8b8e3db97a5583814cf5977878f1c8cfd
Daniel Talik <danieltalik@gmail.com>
2020-05-02 20:22:02 -0400

    Got background and text overlay

--- src/About.js
+++ src/About.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import TITLES from './data/titles';
+import Me from './assets/Me.jpg';
+import galaxy from './assets/galaxy.jpg';
 import './About.css';
 
 class About extends Component {
@@ -19,8 +21,8 @@ class About extends Component {
 
     iterateTitles = () => {
         setInterval(() => {
-         
-            this.setState({ index: (this.state.index + 1) % TITLES.length, fade: true})
+
+            this.setState({ index: (this.state.index + 1) % TITLES.length, fade: true })
             this.fadeOut();
         }, 4000)
     }
@@ -29,11 +31,14 @@ class About extends Component {
         const fadeVal = this.state.fade;
         const title = TITLES[this.state.index];
         return (
-            <div>
-                <h1>Hello There!</h1>
-                
-                <h3>My name is Dan Talik.</h3>
-                <p className={fadeVal ? 'title-fade-in' : 'title-fade-out'}> I am {title}</p>
+            <div className='container'>
+                <img src={galaxy} alt='Milky Way' id='background' />
+                <div className='centered'>
+                    <h1>Hello There!</h1>
+                    <img src={Me} alt='Dan Talik' style={{ height: 300, width: 300, borderRadius: 150, borderColor: 'white', borderStyle: 'solid', borderWidth: 2 }} />
+                    <h3>My name is Dan Talik.</h3>
+                    <p className={fadeVal ? 'title-fade-in' : 'title-fade-out'}> I am {title}</p>
+                </div>
             </div>
         )
     }
